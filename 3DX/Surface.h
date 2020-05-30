aa395485c031cf4cfee30fc1977654e89039e7b4
cagri alp <calp914@gmail.com>
2020-05-30 09:38:37 -0400

    Missing libs added

--- 3DX/Surface.h
+++ 3DX/Surface.h
@@ -1,25 +1,25 @@
-#pragma once
-#include "Colors.h"
-#include <string>
-#include "DirectXTex.h"
-
-class Surface
-{
-public:
-
-	Surface(const std::wstring& filename);
-	Surface(int width, int height);
-	Surface(const Surface&);
-	Surface& operator=(const Surface& rhs);
-	~Surface();
-	void PutPixel(int x, int y, Color c);
-	Color GetPixel(int x, int y) const;
-	const Color* GetColorPointer() const;
-	int GetWidth() const;
-	int GetHeight()const;
-public:
-	int height;
-	int width;
-	DXGI_FORMAT format = DXGI_FORMAT_B8G8R8A8_UNORM;
-	std::unique_ptr<Color[]> pPixel;
-};
+//#pragma once
+//#include "Colors.h"
+//#include <string>
+//#include "DirectXTex.h"
+//
+//class Surface
+//{
+//public:
+//
+//	Surface(const std::wstring& filename);
+//	Surface(int width, int height);
+//	Surface(const Surface&);
+//	Surface& operator=(const Surface& rhs);
+//	~Surface();
+//	void PutPixel(int x, int y, Color c);
+//	Color GetPixel(int x, int y) const;
+//	const Color* GetColorPointer() const;
+//	int GetWidth() const;
+//	int GetHeight()const;
+//public:
+//	int height;
+//	int width;
+//	DXGI_FORMAT format = DXGI_FORMAT_B8G8R8A8_UNORM;
+//	std::unique_ptr<Color[]> pPixel;
+//};
