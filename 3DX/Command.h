1391f3739548eea0fd27397e66cd12a7772783f5
Cagri alp Yaprak <calp914@gmail.com>
2020-06-14 16:33:02 -0700

    multipass renderqueue

---
+++ 3DX/Command.h
@@ -0,0 +1,29 @@
+#pragma once
+
+
+namespace Command
+{
+	struct Draw
+	{
+
+		//backendDispatch function
+		//vertexCount
+		//startindex
+		//vertexbuffer
+		//primitiveTopology
+		//vertexlayout
+
+	};
+	struct IndexedDraw
+	{
+
+		//backendDispatch function
+		//IndexCount
+		//startindex
+		//vertexbuffer
+		//indexBuffer
+		//primitiveTopology
+		//vertexlayout
+
+	};
+}
--- 3DX/CommandPacket.h
+++ 3DX/CommandPacket.h
@@ -2,18 +2,21 @@
 #include "Step.h"
 #include <vector>
 
-class Drawable;
-
-class CommandPacket
+typedef void* CommandPacket;
+namespace commandPacket
 {
+	static const size_t OFFSET_NEXT_COMMAND_PACKET = 0u;
+	static const size_t OFFSET_BACKEND_DISPATCH_FUNCTION = OFFSET_NEXT_COMMAND_PACKET + sizeof(commandPacket);
+	static const size_t OFFSET_COMMAND = OFFSET_BACKEND_DISPATCH_FUNCTION + sizeof(backendDispatchFunction);
 	
-public:
-	
-	CommandPacket(Drawable* drawable);
-	void PushStep(const Step& step);
-	void Bind(Graphics& gfx);
-
-private:
-	class Drawable* pDrawable;
-	std::vector<Step> steps;
+	template<typename T>
+	CommandPacket Create(size_t auxMemorySize)
+	{
+		return ::new_handler(GetSize<T>(auxMemorySize));
+	}
+	template <typename T>
+	size_t GetSize(size_t auxMemorySize)
+	{
+		return OFFSET_COMMAND + sizeof(T) + auxMemorySize;
+	}
 };
